10da78d7a74be94797c489f2606610d5e267d1ea
Ramakrishna541 <ramakrishnamajjiga41@gmail.com>
2026-06-22 22:19:57 +0530

    Add files via upload
    added new file

---
+++ whilecontinue.py
@@ -0,0 +1,21 @@
+# Initialize the counter
+num = 1
+
+# Loop through numbers from 1 to 10
+while num <= 10:
+
+    # Stop the loop when the number reaches to 8
+    if num == 8:
+        print("Encountered 8.")
+        break  # Exits the loop 
+
+    # Check if the number is odd
+    if num % 2 != 0:
+        num += 1
+        continue  # Skip the rest of  the loop and move to the next number
+
+    # Print only even numbers
+    print(num)
+
+    # Increment the counter
+    num += 1
